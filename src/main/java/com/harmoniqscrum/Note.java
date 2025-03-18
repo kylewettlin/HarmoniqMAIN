3f4bb29d527d30daec912492fbc174337242c747
tdoverbay <toverbay@email.sc.edu>
2025-03-18 18:01:10 -0400

    Updated Note class to contain basis of getPitch

--- src/main/java/com/harmoniqscrum/Note.java
+++ src/main/java/com/harmoniqscrum/Note.java
@@ -2,10 +2,44 @@ package com.harmoniqscrum;
 
 public class Note {
     private String pitch;
+    private int string;
+    private int fret;
     private double duration;
     private int volume;
+
+    private String[] notes = {"a","a#","b","c","c#","d","d#","e","f","f#","g","g#"};
+    private String[] tuning = {"e","b","g","d","a","e"};
+    private int octave;
+
+    //Note(6, 0)
+
+    public Note(int string, int fret){
+        this.string = string;
+        this.fret = fret;
+        this.pitch = getPitch();
+    }
 
     public void play(){
 
     } 
+
+    public String getPitch(){
+        String stringN = tuning[string];
+        int startIndex = 0;
+        for (int i = 0; i < notes.length; i++) {
+            if (notes[i].equals(stringN)) {
+                startIndex =  i; // Return the index if the target is found
+            }
+        }
+        int num = startIndex;
+        for(int i = startIndex; i<fret; i++){
+            num++;
+            if(num > notes.length){
+                octave++;
+                num = 0;
+            }
+            //if(bigger than array of notes, octave up and move to beginning)
+        }
+        return pitch;
+    }
 }
