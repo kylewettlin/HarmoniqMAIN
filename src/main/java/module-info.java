573d95bb0873e7225e789cdf4728273f33b35286
KiNgChAzY <158865021+KiNgChAzY@users.noreply.github.com>
2025-03-27 14:15:49 -0400

    junit testing
    added junit and the folder and file need

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.harmoniqscrum {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires junit;
     requires json.simple;
     requires java.desktop;
     
---
+++ src/main/java/test/java/com/model/UsersTest.java
@@ -0,0 +1,7 @@
+package test.java.com.model;
+
+import org.junit.Test;
+
+public class UsersTest {
+    
+}
